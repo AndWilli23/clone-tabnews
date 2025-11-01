feb44f92ba0955f9abcf236b04b3349b5048f7e5
Anderson Willian <150717554+AndWilli23@users.noreply.github.com>
2025-11-01 14:46:25 +0000

    adiona scripst  e

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
-function Home () {
-    return <h1>A vida por mais amarga que seja, ainda continua linda, assim como seus olhos!</h1>
+function Home() {
+  return (
+    <h1>
+      A vida por mais amarga que seja, ainda continua linda, assim como seus
+      olhos!
+    </h1>
+  );
 }
 
 export default Home;

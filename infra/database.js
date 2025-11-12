4041ae9c4a5ea9488c6cdfd8c027112f5e5ef2e9
Anderson Willian <150717554+AndWilli23@users.noreply.github.com>
2025-11-12 01:12:15 +0000

    database mais robusto para lidar com erros

--- infra/database.js
+++ infra/database.js
@@ -1,18 +1,25 @@
-import { Client } from "pg";
+import { Pool } from "pg";
+
+const pool = new Pool({
+  host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT,
+  user: process.env.POSTGRES_USER,
+  database: process.env.POSTGRES_DB,
+  password: process.env.POSTGRES_PASSWORD,
+});
 
 async function query(queryobject) {
-  const client = new Client({
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-  });
-  await client.connect();
-  const result = await client.query(queryobject);
-  await client.end();
+  const client = await pool.connect();
 
-  return result;
+  try {
+    const result = await client.query(queryobject);
+    return result;
+  } catch (err) {
+    console.error(err);
+    throw err;
+  } finally {
+    client.release();
+  }
 }
 
 export default {
